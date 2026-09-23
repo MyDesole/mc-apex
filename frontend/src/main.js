ed97347eaf5e864a9e3ba921300abd52cb26cf74
Nikita <nikita.kolygin@iss.ru>
2026-09-23 20:46:03 +0300

    auth

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
+import '@fontsource/inter/800.css'
+import '@fontsource/inter/900.css'
 import './assets/base.css'
 import './assets/main.css'
 
